1b79008528a4c931c6c6816c6409883da819523f
Jano <bonnat.jano.epet20@gmail.com>
2022-11-05 19:26:51 -0300

    sse crea variable para saber en qué pasos estamos

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,5 @@ const progreso = document.getElementById('progreso');
 const anterior = document.getElementById('anterior');
 const siguiente = document.getElementById('siguiente');
 const circulos = document.querySelectorAll('.circle');
+
+currentActive = 1; //variable para saber a qé nivel de los pasos vamos
