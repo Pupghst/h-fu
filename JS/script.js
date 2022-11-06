0e1d49d483c13878a6938d62f8a9a9c5edcf309d
Jano <bonnat.jano.epet20@gmail.com>
2022-11-06 12:32:44 -0300

    se crea funcion que aumenta cada que se hace click

--- JS/script.js
+++ JS/script.js
@@ -3,4 +3,8 @@ const anterior = document.getElementById('anterior');
 const siguiente = document.getElementById('siguiente');
 const circulos = document.querySelectorAll('.circle');
 
-currentActive = 1; //variable para saber a qé nivel de los pasos vamos
+currentActive = 1; //variable para saber a qué nivel de los pasos vamos
+
+siguiente.addEventListener('click', () =>{ //echucha el click y ejecuta una funcion
+    currentActive++;
+});
