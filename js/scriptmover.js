910447ee6922b05bea8fb88a57d5c5d4c466313c
Santiagocornu <101955412+Santiagocornu@users.noreply.github.com>
2022-11-13 20:48:42 -0300

    asociando h1 con nombre

--- js/scriptmover.js
+++ js/scriptmover.js
@@ -4,5 +4,5 @@ let numero=document.getElementById('numero');
 function crearcomentario(nombre){
 let capa = document.getElementsByClassName('capa');
 let h1= document.createElement('h1');
-
+h1.innerHTML = nombre;
 }
