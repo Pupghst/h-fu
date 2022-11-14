04816e0ac372440b2f33c1ecbe83743659453fde
Santiagocornu <101955412+Santiagocornu@users.noreply.github.com>
2022-11-14 14:32:31 -0300

    acortando codigo

--- js/scriptcomentario.js
+++ js/scriptcomentario.js
@@ -1,12 +1,17 @@
 
 function crearcomentario(texto,texto2){
+
+
 if(texto.length>=1 && texto2.length>=1){
+element = document.getElementById('submit');
 a1= "ya te mandaremos un mensaje " + texto;
 document.getElementById("capa").innerHTML=a1;
+
+element.style.property('color','green');
+
 }else{
     if(texto.length<1){
     element = document.getElementById('submit');
-    submit2 = document.getElementsByClassName("submit2");
     a1= "no introdujo nombre";
     document.getElementById("capa").innerHTML=a1;
     element.classList.add("submit2");
@@ -14,10 +19,10 @@ document.getElementById("capa").innerHTML=a1;
     
     }else{
     element = document.getElementById('submit');
-    submit2 = document.getElementsByClassName("submit2");
     a1= "no introdujo numero";
     document.getElementById("capa").innerHTML=a1;
     element.classList.add("submit2");
     }
 }
+
 }
