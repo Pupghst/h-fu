1ed530badda217c84cbcfa2779748c6b8e06ddc6
Santiagocornu <101955412+Santiagocornu@users.noreply.github.com>
2022-11-13 21:23:16 -0300

    añadiendo mensaje a a1

--- js/scriptmover.js
+++ js/scriptmover.js
@@ -1,7 +1,11 @@
 
 function crearcomentario(texto){
+
 capa = document.getElementById("capa");
 h1 = document.createElement("h1");
-h1.innerHTML = texto;
+a1= "ya te mandaremos un mensaje " + texto;
+h1.innerHTML = a1;
 capa.appendChild(h1);
+hola=2;
+
 }
