7a49346bc36df4916be1efed78a750e8d0307988
Santiagocornu <101955412+Santiagocornu@users.noreply.github.com>
2022-11-14 15:13:55 -0300

    solucionando problemas de compatibilidad

--- js/scriptcomentario.js
+++ js/scriptcomentario.js
@@ -1,15 +1,34 @@
 
 function crearcomentario(texto,texto2){
+    element = document.getElementById('submit');
+    element.style.setProperty("color", "");
+    element.classList.remove("submit2");
 if(texto.length>=1 && texto2.length>=1){
+
+element.classList.remove("submit2");
 a1= "ya te mandaremos un mensaje " + texto;
 document.getElementById("capa").innerHTML=a1;
+element.classList.add("submit");
+element.style.setProperty("color", "green");
+
+
+
 }else{
     if(texto.length<1){
+   
+    element.classList.remove("submit");
     a1= "no introdujo nombre";
     document.getElementById("capa").innerHTML=a1;
+    element.classList.add("submit2");
+    
+    
     }else{
+    
+    element.classList.remove("submit");
     a1= "no introdujo numero";
     document.getElementById("capa").innerHTML=a1;
+    element.classList.add("submit2");
     }
 }
+
 }
