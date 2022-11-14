8d6250d7d8e1541d832723f48c17600f2649347c
Santiagocornu <101955412+Santiagocornu@users.noreply.github.com>
2022-11-14 14:19:22 -0300

    acortando codigo

--- js/scriptcomentario.js
+++ js/scriptcomentario.js
@@ -6,25 +6,18 @@ document.getElementById("capa").innerHTML=a1;
 }else{
     if(texto.length<1){
     element = document.getElementById('submit');
+    submit2 = document.getElementsByClassName("submit2");
     a1= "no introdujo nombre";
     document.getElementById("capa").innerHTML=a1;
-    element.style.setProperty('color','red');
-    element.style.setProperty( 'transform','scale(0.98s)');
-    element.style.setProperty('position','relative');
-    element.style.setProperty('animation-name','mover_boton');
-    element.style.setProperty('animation-duration','0.1s'),
-    element.style.setProperty('animation-timing-function', 'linear');
+    element.classList.add("submit2");
+    
     
     }else{
-        element = document.getElementById('submit');
+    element = document.getElementById('submit');
+    submit2 = document.getElementsByClassName("submit2");
     a1= "no introdujo numero";
     document.getElementById("capa").innerHTML=a1;
-    element.style.setProperty('color','red');
-    element.style.setProperty( 'transform','scale(0.98s)');
-    element.style.setProperty('position','relative');
-    element.style.setProperty('animation-name','mover_boton');
-    element.style.setProperty('animation-duration','0.1s'),
-    element.style.setProperty('animation-timing-function', 'linear');
+    element.classList.add("submit2");
     }
 }
 }
