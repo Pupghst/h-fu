fdecd07c51208b6108f206d23ae2150cb48560af
Santiagocornu <101955412+Santiagocornu@users.noreply.github.com>
2022-11-14 14:42:23 -0300

    solucionando problemas con animacion

--- js/scriptcomentario.js
+++ js/scriptcomentario.js
@@ -1,19 +1,21 @@
 
 function crearcomentario(texto,texto2){
-
-
+    element = document.getElementById('submit');
+    element.style.setProperty("color", "");
+    element.classList.remove("submit2");
 if(texto.length>=1 && texto2.length>=1){
-element = document.getElementById('submit');
+
 element.classList.remove("submit2");
 a1= "ya te mandaremos un mensaje " + texto;
 document.getElementById("capa").innerHTML=a1;
 element.classList.add("submit");
+element.style.setProperty("color", "green");
 
 
 
 }else{
     if(texto.length<1){
-    element = document.getElementById('submit');
+   
     element.classList.remove("submit");
     a1= "no introdujo nombre";
     document.getElementById("capa").innerHTML=a1;
@@ -21,7 +23,7 @@ element.classList.add("submit");
     
     
     }else{
-    element = document.getElementById('submit');
+    
     element.classList.remove("submit");
     a1= "no introdujo numero";
     document.getElementById("capa").innerHTML=a1;
