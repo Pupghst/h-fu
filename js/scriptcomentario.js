5bc6e3308a9b937490dd029c5f4ab2b787bba26d
Santiagocornu <101955412+Santiagocornu@users.noreply.github.com>
2022-11-14 14:34:33 -0300

    realizando remove y add class

--- js/scriptcomentario.js
+++ js/scriptcomentario.js
@@ -4,14 +4,17 @@ function crearcomentario(texto,texto2){
 
 if(texto.length>=1 && texto2.length>=1){
 element = document.getElementById('submit');
+element.classList.remove("submit2");
 a1= "ya te mandaremos un mensaje " + texto;
 document.getElementById("capa").innerHTML=a1;
+element.classList.add("submit");
 
-element.style.property('color','green');
+
 
 }else{
     if(texto.length<1){
     element = document.getElementById('submit');
+    element.classList.remove("submit");
     a1= "no introdujo nombre";
     document.getElementById("capa").innerHTML=a1;
     element.classList.add("submit2");
@@ -19,6 +22,7 @@ element.style.property('color','green');
     
     }else{
     element = document.getElementById('submit');
+    element.classList.remove("submit");
     a1= "no introdujo numero";
     document.getElementById("capa").innerHTML=a1;
     element.classList.add("submit2");
