057e83ce270ef60902e5953352c0397cff97d4b2
Santiagocornu <101955412+Santiagocornu@users.noreply.github.com>
2022-11-13 21:06:12 -0300

    cambiando mayusculas

--- js/scriptmover.js
+++ js/scriptmover.js
@@ -1,9 +1,7 @@
-let nombre=document.getElementById('nombre');
-let numero=document.getElementById('numero');
 
-function crearcomentario(nombre){
-let capa = document.getElementsByClassName('capa');
-let h1= document.createElement('h1');
-h1.innerHTML = nombre;
+function crearcomentario(texto){
+capa = document.getElementById("capa");
+h1 = document.createElement("h1");
+h1.innerHTML = texto;
 capa.appendChild(h1);
 }
