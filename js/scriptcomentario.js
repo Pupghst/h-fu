67c7b632f9274e26b7baa389f467a5af3fc657fd
Santiagocornu <101955412+Santiagocornu@users.noreply.github.com>
2022-11-14 09:13:41 -0300

    cambiando codigo para cambiar h1

--- js/scriptcomentario.js
+++ js/scriptcomentario.js
@@ -1,24 +1,15 @@
 
 function crearcomentario(texto,texto2){
 if(texto.length>=1 && texto2.length>=1){
-capa = document.getElementById("capa");
-h1 = document.createElement("h1");
 a1= "ya te mandaremos un mensaje " + texto;
-h1.innerHTML = a1;
-capa.appendChild(h1);
+document.getElementById("capa").innerHTML=a1;
 }else{
     if(texto.length<1){
-    capa = document.getElementById("capa");
-    h1 = document.createElement("h1");
     a1= "no introdujo nombre";
-    h1.innerHTML = a1;
-    capa.appendChild(h1);
+    document.getElementById("capa").innerHTML=a1;
     }else{
-        capa = document.getElementById("capa");
-    h1 = document.createElement("h1");
     a1= "no introdujo numero";
-    h1.innerHTML = a1;
-    capa.appendChild(h1);
+    document.getElementById("capa").innerHTML=a1;
     }
 }
 }
