f38c2f8ba185abe40fd7c79d2395314234e20ec1
Santiagocornu <101955412+Santiagocornu@users.noreply.github.com>
2022-11-14 14:11:31 -0300

    añadiendo propiedades en if

--- js/scriptcomentario.js
+++ js/scriptcomentario.js
@@ -5,11 +5,26 @@ a1= "ya te mandaremos un mensaje " + texto;
 document.getElementById("capa").innerHTML=a1;
 }else{
     if(texto.length<1){
+    element = document.getElementById('submit');
     a1= "no introdujo nombre";
     document.getElementById("capa").innerHTML=a1;
+    element.style.setProperty('color','red');
+    element.style.setProperty( 'transform','scale(0.98s)');
+    element.style.setProperty('position','relative');
+    element.style.setProperty('animation-name','mover_boton');
+    element.style.setProperty('animation-duration','0.1s'),
+    element.style.setProperty('animation-timing-function', 'linear');
+    
     }else{
+        element = document.getElementById('submit');
     a1= "no introdujo numero";
     document.getElementById("capa").innerHTML=a1;
+    element.style.setProperty('color','red');
+    element.style.setProperty( 'transform','scale(0.98s)');
+    element.style.setProperty('position','relative');
+    element.style.setProperty('animation-name','mover_boton');
+    element.style.setProperty('animation-duration','0.1s'),
+    element.style.setProperty('animation-timing-function', 'linear');
     }
 }
 }
