dce0f51cbf7a0f2107c02b87380ed24eda52ee4f
foxma <foxmanlol508@gmail.com>
2022-11-16 18:38:10 -0300

    generando movimiento para .formulario

--- js/scriptcomentario.js
+++ js/scriptcomentario.js
@@ -1,38 +1,38 @@
 function eliminarclass(){
-    element.classList.remove("submit2");
+    element.classList.remove("errorsubmit");
     element.classList.add("submit");
+    parent.classList.remove("errorpagina");
 }
 function crearcomentario(texto,texto2){
     element = document.getElementById('submit');
+    parent = document.getElementById('parent');
     element.style.setProperty("color", "");
-    element.classList.remove("submit2");
-if(texto.length>=1 && texto2.length>=1){
-
-element.classList.remove("submit2");
-a1= "ya te mandaremos un mensaje " + texto;
-document.getElementById("capa").innerHTML=a1;
-element.classList.add("submit");
-element.style.setProperty("color", "green");
-
-
-
-}else{
-    if(texto.length<1){
-    element.classList.remove("submit");
-    a1= "no introdujo nombre";
-    document.getElementById("capa").innerHTML=a1;
-    element.classList.add("submit2");
-    setTimeout(eliminarclass,100);
-
-    
-    
+    element.classList.remove("errorsubmit");
+    if(texto.length>=1 && texto2.length>=1){
+        element.classList.remove("errorsubmit");
+        a1= "ya te mandaremos un mensaje " + texto;
+        document.getElementById("capa").innerHTML=a1;
+        element.classList.add("submit");
+        element.style.setProperty("color", "green");
     }else{
-    element.classList.remove("submit2");
-    element.classList.remove("submit");
-    a1= "no introdujo numero";
-    document.getElementById("capa").innerHTML=a1;
-    element.classList.add("submit2");
-    setTimeout(eliminarclass,100);
+        if(texto.length<1){
+            element.classList.remove("submit");
+            a1= "no introdujo nombre";
+            document.getElementById("capa").innerHTML=a1;
+        element.classList.add("errorsubmit");
+        parent.classList.add("errorpagina");
+        setTimeout(eliminarclass,100);
+
+    
+    
+        }else{
+            element.classList.remove("errorsubmit");
+            element.classList.remove("submit");
+            a1= "no introdujo numero";
+            document.getElementById("capa").innerHTML=a1;
+            element.classList.add("errorsubmit");
+            parent.classList.add("errorpagina");
+            setTimeout(eliminarclass,100);
     }
 }
 
