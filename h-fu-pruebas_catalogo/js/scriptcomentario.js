9c27a3a85dcad7f4d98227bb6f8aa6dd47e03a4e
xiaraa <xiararojido04@gmail.com>
2022-12-10 02:19:23 -0300

    element

--- h-fu-pruebas_catalogo/js/scriptcomentario.js
+++ h-fu-pruebas_catalogo/js/scriptcomentario.js
@@ -3,3 +3,10 @@ function crearcomentario(texto,texto2){
     element.style.setProperty("color", "");
     element.classList.remove("submit2");
 if(texto.length>=1 && texto2.length>=1){
+    
+element.classList.remove("submit2");
+a1= "ya te mandaremos un mensaje " + texto;
+document.getElementById("capa").innerHTML=a1;
+element.classList.add("submit");
+element.style.setProperty("color", "green");
+
