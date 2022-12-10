c0479b31b96ab085b3c7238f5e8cdfbbc6443b53
xiaraa <xiararojido04@gmail.com>
2022-12-10 02:18:58 -0300

    creando js

---
+++ h-fu-pruebas_catalogo/js/scriptcomentario.js
@@ -0,0 +1,5 @@
+function crearcomentario(texto,texto2){
+    element = document.getElementById('submit');
+    element.style.setProperty("color", "");
+    element.classList.remove("submit2");
+if(texto.length>=1 && texto2.length>=1){
